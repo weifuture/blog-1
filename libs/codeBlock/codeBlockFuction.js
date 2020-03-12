7f15a312b8eafc1c967575fa9024f735faae637d
weifuture <weifuture@users.noreply.github.com>
2020-03-12 04:00:58 +0000

    10002 weifuture/blog-hexo@f166617d858d495ea6d82f4c096ff9cd9a51a4ad

---
+++ libs/codeBlock/codeBlockFuction.js
@@ -0,0 +1,3 @@
+// build time:Thu Mar 12 2020 04:00:55 GMT+0000 (Coordinated Universal Time)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
